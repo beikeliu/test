8f87dfd3918a4febe129e02a8529b1e446097d7a
v_beileiliu <v_beileiliu@tencent.com>
2023-06-20 17:22:12 +0800

    update

---
+++ useCountdown.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useCountdown = (init = 10) => {
+  if (!(init > 1)) {
+    throw new RangeError("useCountdown requires init > 1");
+  }
+  const [count, setCount] = useState(init);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => {
+        if (count <= 0) {
+          clearInterval(timer);
+          return count;
+        }
+        return count - 1;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  return count;
+};
+export default useCountdown;
